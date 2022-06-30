fbe545c94ef265146e075e9a96d525b214029aa6
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2022-06-30 14:14:36 -0300

    Initial commit

---
+++ src/js/script.js
@@ -0,0 +1,5 @@
+const tempo = document.getElementById('tempo')
+const iniciar = document.querySelectorAll('.bt')[0]
+const parar = document.querySelectorAll('.bt')[1]
+const reset = document.querySelectorAll('.bt')[2]
+
